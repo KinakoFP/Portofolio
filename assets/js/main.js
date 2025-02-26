68f1270338a3e5afaa59b3240ba92e1479bca876
Kinako <gaetan.muller2005@gmail.com>
2025-02-26 09:41:45 +0100

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
 
     // Full stack proficiency years appear in the footer
      
-    $('#fullstack-years').text('2018 - ' + new Date().getFullYear());
+    $('#fullstack-years').text(new Date().getFullYear());
     
 });
